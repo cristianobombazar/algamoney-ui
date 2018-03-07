fcaa2c409ac529ea7422addc220f66cc5a5a9f6d
cristianobombazar <31328601+cristianobombazar@users.noreply.github.com>
2018-03-06 21:55:17 -0300

    10.8. Adicionando tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {AccordionModule} from 'primeng/primeng';
+import {AccordionModule } from 'primeng/primeng';
 
 import {InputTextModule} from 'primeng/components/inputtext/inputtext';
 import {ButtonModule} from 'primeng/components/button/button';
 import { DataTableModule } from 'primeng/components/datatable/datatable';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+
 
 
 @NgModule({
@@ -17,7 +19,8 @@ import { DataTableModule } from 'primeng/components/datatable/datatable';
     ButtonModule,
     AccordionModule,
     InputTextModule,
-    DataTableModule
+    DataTableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
