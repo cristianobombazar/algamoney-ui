cac51fe0fe6d9e931035e1fa3b87f3dab0618e25
francileneboeing <francileneboeing@hotmail.com>
2018-03-30 16:01:20 -0300

    13.8. Validando controles de formulário com PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import {AccordionModule, CalendarModule, DropdownModule, InputMaskModule, InputTextareaModule, SelectButtonModule} from 'primeng/primeng';
 
-import {InputTextModule} from 'primeng/components/inputtext/inputtext';
-import {ButtonModule} from 'primeng/components/button/button';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { ButtonModule } from 'primeng/components/button/button';
 import { DataTableModule } from 'primeng/components/datatable/datatable';
 import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
+import { CalendarModule } from 'primeng/components/calendar/calendar';
+import { SelectButtonModule } from 'primeng/components/selectbutton/selectbutton';
+import { DropdownModule } from 'primeng/components/dropdown/dropdown';
+import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
+
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+
+import { AppComponent } from './app.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { PessoaPesquisaComponent } from './pessoa-pesquisa/pessoa-pesquisa.component';
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {CurrencyMaskModule} from 'ng2-currency-mask';
 import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
-
-
+import {PessoaPesquisaComponent} from './pessoa-pesquisa/pessoa-pesquisa.component';
 
 @NgModule({
   declarations: [
@@ -28,18 +33,20 @@ import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.compo
   ],
   imports: [
     BrowserModule,
-    ButtonModule,
-    AccordionModule,
+    BrowserAnimationsModule,
+    FormsModule,
+
     InputTextModule,
+    ButtonModule,
     DataTableModule,
     TooltipModule,
     InputTextareaModule,
     CalendarModule,
-    BrowserAnimationsModule,
     SelectButtonModule,
     DropdownModule,
-    CurrencyMaskModule,
-    InputMaskModule
+    InputMaskModule,
+
+    CurrencyMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
