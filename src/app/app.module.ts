537b4c23d5fb01f2c1220aabaa5bc0fbfbb1019e
francileneboeing <francileneboeing@hotmail.com>
2018-03-30 12:03:39 -0300

    13.3. Adicionando botão de seleção

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {AccordionModule, CalendarModule, InputTextareaModule} from 'primeng/primeng';
+import {AccordionModule, CalendarModule, InputTextareaModule, SelectButtonModule} from 'primeng/primeng';
 
 import {InputTextModule} from 'primeng/components/inputtext/inputtext';
 import {ButtonModule} from 'primeng/components/button/button';
@@ -32,7 +32,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     TooltipModule,
     InputTextareaModule,
     CalendarModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SelectButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
