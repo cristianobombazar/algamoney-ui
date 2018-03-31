b368454b954a943264b19b22a141028bf9ef7225
francileneboeing <francileneboeing@hotmail.com>
2018-03-30 21:14:54 -0300

    14.11. Criando o Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
 import {LancamentosModule} from './lancamentos/lancamentos.module';
 import {PessoasModule} from './pessoas/pessoas.module';
 import {MessageModule} from 'primeng/message';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     LancamentosModule,
     PessoasModule,
-    MessageModule
+    MessageModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
