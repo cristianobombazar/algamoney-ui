30a7f8079ef424377e6340fc53f550642f16c454
francileneboeing <francileneboeing@hotmail.com>
2018-04-01 23:30:48 -0300

    17.7. Desafio: criando a consulta e listagem de pessoas

---
+++ src/app/pessoas/pessoa.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/toPromise';
+import {Headers, Http, URLSearchParams} from '@angular/http';
+
+
+export class FiltroPessoa {
+  nome: string;
+  pagina = 0;
+  itensPorPagina = 5;
+}
+
+@Injectable()
+export class PessoaService {
+
+  URI = 'http://localhost:8080/pessoa';
+
+  constructor(private http: Http) { }
+
+  pesquisar(filtro: FiltroPessoa): Promise<any> {
+    const headers = new Headers();
+    headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    const params = new URLSearchParams();
+    params.set('page', filtro.pagina.toString());
+    params.set('size', filtro.itensPorPagina.toString());
+    if (filtro.nome) {
+      params.set('nome', filtro.nome);
+    }
+    return this.http.get(this.URI + '?', {headers, search : params})
+                     .toPromise()
+                     .then( response =>{
+                       const responseJson = response.json();
+                       const pessoas  = responseJson.content;
+                       const result = {
+                         pessoas: pessoas,
+                         total : responseJson.totalElements
+                       }
+                       return result;
+                     });
+  }
+
+  listarTodas(): Promise<any> {
+    const headers = new Headers();
+    headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    return this.http.get(this.URI + '?', {headers})
+      .toPromise()
+      .then( response => response.json().content);
+  }
+
+}
--- src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {FiltroPessoa, PessoaService} from '../pessoa.service';
+import {LazyLoadEvent} from 'primeng/components/common/api';
 
 @Component({
   selector: 'app-pessoas-pesquisa',
   templateUrl: './pessoas-pesquisa.component.html',
   styleUrls: ['./pessoas-pesquisa.component.css']
 })
-export class PessoasPesquisaComponent {
+export class PessoasPesquisaComponent implements OnInit{
 
-  pessoas = [
-    { nome: 'Manoel Pinheiro', cidade: 'Uberlândia', estado: 'MG', ativo: true },
-    { nome: 'Sebastião da Silva', cidade: 'São Paulo', estado: 'SP', ativo: false },
-    { nome: 'Carla Souza', cidade: 'Florianópolis', estado: 'SC', ativo: true },
-    { nome: 'Luís Pereira', cidade: 'Curitiba', estado: 'PR', ativo: true },
-    { nome: 'Vilmar Andrade', cidade: 'Rio de Janeiro', estado: 'RJ', ativo: false },
-    { nome: 'Paula Maria', cidade: 'Uberlândia', estado: 'MG', ativo: true }
-  ];
+  totalRegistros = 0;
+  pessoas = [];
+  filtro = new FiltroPessoa();
+
+  constructor(private pessoaService: PessoaService) {}
+
+  ngOnInit(): void {
+  }
+
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
+    this.pessoaService.pesquisar(this.filtro)
+                      .then(resultado => {
+                          this.totalRegistros = resultado.total;
+                          this.pessoas = resultado.pessoas;
+                      });
+  }
+
+  onChangePage(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
+  }
+
+
 
 }
