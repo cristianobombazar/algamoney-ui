11756711a2a810d1cad20388d9125882b48afb46
francileneboeing <francileneboeing@hotmail.com>
2018-04-01 22:42:53 -0300

    17.6. Configurando a paginação lazy do PrimeNG

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -9,7 +9,7 @@ export class LancamentoFiltro {
   dataVencimentoInicio: Date;
   dataVencimentoFim: Date;
   pagina = 0;
-  itensPorPagina = 5;
+  itensPorPagina = 3;
 }
 
 @Injectable()
--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,5 +1,6 @@
 import {LancamentoFiltro, LancamentoService} from './../lancamento.service';
 import { Component, OnInit } from '@angular/core';
+import {LazyLoadEvent} from 'primeng/components/common/api';
 
 @Component({
   selector: 'app-lancamentos-pesquisa',
@@ -8,20 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentosPesquisaComponent implements OnInit {
 
-  lancamentos = [];
-  filtro = new LancamentoFiltro();
+  totalRegistros = 0;
+  lancamentos    = [];
+  filtro         = new LancamentoFiltro();
 
   constructor(private lancamentoService: LancamentoService) { }
 
   ngOnInit() {
-    this.pesquisar();
   }
 
-  pesquisar() {
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
     this.lancamentoService.pesquisar(this.filtro)
                           .then(resultado => {
+                                this.totalRegistros = resultado.total;
                                 this.lancamentos = resultado.lancamentos;
                           });
   }
 
+  onChangePage(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
+  }
 }
