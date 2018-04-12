3cbf08ac1bf57f55b824678e3be5e1e970e1d69f
francileneboeing <francileneboeing@hotmail.com>
2018-04-11 21:22:12 -0300

    18.13. Refatorando as rotas para usar Routing Module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+import {LancamentosPesquisaComponent} from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
+import {LancamentoCadastroComponent} from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import {PessoasPesquisaComponent} from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+import {PaginaNaoEncontradaComponent} from './core/pagina-nao-encontrada.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'lancamentos', pathMatch: 'full'},
+  {path: 'lancamentos', component: LancamentosPesquisaComponent},
+  {path: 'lancamentos/novo', component: LancamentoCadastroComponent},
+  {path: 'lancamentos/:id', component: LancamentoCadastroComponent},
+  {path: 'pessoas', component: PessoasPesquisaComponent},
+  {path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent},
+  {path: '**', redirectTo: 'pagina-nao-encontrada'}, // ** SIGNIFICA QUE NAO FOI ENCONTRADO
+];
+
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule] // exporta o routermodule para poder utilizar a diretiva routerlink no app component
+})
+export class AppRoutingModule { }
