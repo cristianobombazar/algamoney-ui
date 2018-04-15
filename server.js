fb6dc909dd44813108bbe4e76cd2b64f85a64f54
francileneboeing <francileneboeing@hotmail.com>
2018-04-15 13:31:55 -0300

    20.3. Respondendo requisições com Node.js e Express

---
+++ server.js
@@ -0,0 +1,11 @@
+// PARA COLOCAR EM PRODUÇÃO OS ARQUIVOS DIST
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+app.listen(4200);
